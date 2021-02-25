9404b904ac891d0969dde9cdcd9a5019c5486aee
juliasherbal <sherbaljulia@gmail.com>
2021-02-25 16:01:28 -0500

    add name

--- main.js
+++ main.js
@@ -3,6 +3,4 @@
  selectElement(".side-menu-icon").addEventListener("click", ()=>{
 	selectElement(".nav-list").classList.toggle("active");
 	selectElement(".side-menu-icon").classList.toggle("toggle");
-	
-	
  });
