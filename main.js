877ddbcee9072d658c7ebf3e4d61749947fc060c
juliasherbal <sherbaljulia@gmail.com>
2021-02-25 13:24:23 -0500

    nav menu!

---
+++ main.js
@@ -0,0 +1,8 @@
+ const selectElement = (s) => document.querySelector(s);
+ const navLinks = document.querySelectorAll(".nav-link");
+ selectElement(".side-menu-icon").addEventListener("click", ()=>{
+	selectElement(".nav-list").classList.toggle("active");
+	selectElement(".side-menu-icon").classList.toggle("toggle");
+	
+	navLinks.forEach(link, index)
+ });
